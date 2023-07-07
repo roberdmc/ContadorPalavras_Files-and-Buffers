26075544d9d911c30c9f7862902e794d3c383bae
RoberDMC <35946602+roberdmc@users.noreply.github.com>
2023-07-07 11:07:51 -0300

    Fix imports

--- ContadorPalavras.java
+++ ContadorPalavras.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ContadorPalavras {
     public static void main(String[] args) {
     String inputFile = "input.txt";
